1e3b3f72ce59092db3224e45235273aafeac0ac1
Sane69 <40135982+Sane69@users.noreply.github.com>
2023-03-16 17:15:57 +0200

    update first three section

---
+++ assets/main.js
@@ -0,0 +1,53 @@
+var swiper = Swiper;
+var init = false;
+
+function swiperLogo() {
+  if (window.innerWidth <= 1200) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".js-discount__icons-swiper", {
+        slidesPerView: 3,
+        spaceBetween: 15,
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'progressbar'
+        },
+        breakpoints: {
+          767: {
+            slidesPerView: 5
+          }
+        }
+      });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+swiperLogo();
+window.addEventListener("resize", swiperLogo);
+
+var swiperBrand = Swiper;
+var initBrands = false;
+
+function swiperBrands() {
+  if (window.innerWidth <= 1200) {
+    if (!initBrands) {
+      initBrands = true;
+      swiperBrand = new Swiper(".js-brands__info-swiper", {
+        slidesPerView: 1.8,
+        spaceBetween: 15,
+        breakpoints: {
+          767: {
+            slidesPerView: 6
+          }
+        }
+      });
+    }
+  } else if (initBrands) {
+    swiperBrand.destroy();
+    initBrands = false;
+  }
+}
+swiperBrands();
+window.addEventListener("resize", swiperBrands);
